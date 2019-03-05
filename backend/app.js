64ca85ad372e6d11fb3fc6df6644de809dd66bae
Andrew Bateman <gomezbateman@yahoo.com>
2019-03-05 21:40:51 +0100

    app routing & image uploading added

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Post = require('./models/post');
+const postsRoutes = require('./routes/posts')
 
 const app = express();
 
 mongoose
   .connect(
-  "mongodb+srv://Andrew:Spider01@cluster0-pl5l8.mongodb.net/node-angular?retryWrites=true", 
+  "mongodb+srv://Andrew:Spider01@cluster0-pl5l8.mongodb.net/node-angular?retryWrites=true",
   { useNewUrlParser: true }
   )
   .then(() => {
@@ -24,48 +24,16 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    "Access-Control-Allow-Headers", 
+    "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   res.setHeader(
-    "Access-Control-Allow-Methods", 
-    "GET, POST, PATCH, DELETE, OPTIONS"
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
   );
   next();
 });
 
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  post.save().then(result => {
-    res.status(201).json({
-      message: 'Post added successfully',
-      postId: createdPost._id
-    });
-  });
-
-});
-
-app.get("/api/posts", (req, res, next) => {
-  Post
-    .find()
-    .then(documents => {
-      res.status(200).json({
-        message: 'posts fetched successfully',
-        posts: documents
-      });
-    });
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post
-    .deleteOne({_id: req.params.id})
-    .then(result => {
-      console.log(result);
-      res.status(200).json({message: 'post deleted'});
-    });
-});
+app.use("/api/posts", postsRoutes);
 
 module.exports = app;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import {
   MatInputModule,
   MatCardModule,
   MatButtonModule,
   MatToolbarModule,
-   } from '@angular/material'
+  MatSpinner,
+  MatProgressSpinnerModule,
+   } from '@angular/material';
 import {MatExpansionModule} from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 
 @NgModule({
@@ -26,13 +30,15 @@ import { PostListComponent } from './posts/post-list/post-list.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
     MatExpansionModule,
+    MatProgressSpinnerModule,
     HttpClientModule
   ],
   providers: [],
