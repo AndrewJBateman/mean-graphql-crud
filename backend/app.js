a7bd39df20d05f8132f9a5f035c31d57d335919f
AndrewJBateman <gomezbateman@yahoo.com>
2022-02-18 12:19:24 +0100

    fix: correct mongodb connection string

--- backend/app.js
+++ backend/app.js
@@ -13,28 +13,26 @@ app.use(express.json()); // To parse the incoming requests with JSON payloads
 app.use(cors());
 
 const config = require("./config/config.json");
-const dbConnString = `mongodb+srv://${config.user}:` +
-  `${config.password}@cluster0.pl5l8.mongodb.net/` +
+const dbConnString =
+  `mongodb+srv://${config.user}:` +
+  `${config.password}@${config.cluster}.mongodb.net/` +
   `${config.dbName}?retryWrites=true&w=majority`;
+	console.log('dbString: ', dbConnString);
 
 // route to access GraphQL in browser
 app.use(
-	"/graphql",
-	graphqlHTTP({
-		schema: graphqlSchema,
-		rootValue: graphqlResolver,
-		graphiql: true,
-	})
+  "/graphql",
+  graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true,
+  })
 );
 
 // connection to MongoDB
 mongoose
-	.connect(dbConnString, {
-		useUnifiedTopology: true,
-		useNewUrlParser: true,
-		useCreateIndex: true,
-	})
-	.then(() => {
-		app.listen(3000, console.log("Connected to Port 3000."));
-	})
-	.catch((err) => console.log(err));
+  .connect(dbConnString, {})
+  .then(() => {
+    app.listen(3000, console.log("Connected to Port 3000."));
+  })
+  .catch((err) => console.log(err));
